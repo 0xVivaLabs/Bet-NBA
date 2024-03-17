e8bb0ea1609818e740cff7cab4cccbdd40d5a4cf
GlacierLuo <1090490148@qq.com>
2024-03-18 01:55:59 +0800

    Add nextui.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from '@nextui-org/react'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">Bet-NBA
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Bet-NBA
+      <Button color={'primary'}>Submit</Button>
     </main>
   );
 }
