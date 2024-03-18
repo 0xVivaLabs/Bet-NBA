ab2a60b1607e1e35e857f11c00aaee1da2545718
GlacierLuo <1090490148@qq.com>
2024-03-18 08:46:11 +0800

    Set date to 2024-03-18.

--- src/app/today/route.ts
+++ src/app/today/route.ts
@@ -1,7 +1,7 @@
 export async function GET() {
   const date = new Date().toISOString().replace(/T.+/, '')
   // const matches = await fetch(`https://api.balldontlie.io/v1/games?dates[]=${date}`, {
-  const matches = await fetch(`https://api.balldontlie.io/v1/games?dates[]=2024-03-17`, {
+  const matches = await fetch(`https://api.balldontlie.io/v1/games?dates[]=2024-03-18`, {
     headers: {
       Authorization: '25047ffb-eb72-4d42-9b45-9ae3b9685ee5'
     }
